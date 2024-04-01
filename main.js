3bd3deb0d21b288194db1ceeda461a1d4ac228c0
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-04-01 15:32:39 +0530

    Fixed build errors

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -20,5 +18,3 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
